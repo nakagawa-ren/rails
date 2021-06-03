d58d07772675a0f4144c8f4d4bf63bc22eeb6d87
EC2 Default User <ec2-user@ip-172-31-0-187.ec2.internal>
2021-06-03 12:05:12 +0000

    rails-6

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
  get 'todolists' => 'todolists#index'
  #.../todolists/1や.../todolist/3に該当する
  get 'todolists/:id' => 'todolists#show',as:'todolist'
+ get 'todolists/:id/edit' => 'todolists#edit',as:'edit_todolist'
+ patch 'todolists/:id' => 'todolists#update',as:'update_todolist'
 end
