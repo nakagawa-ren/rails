d233cafd29aeaccdb59803e5c13e12dc51c08e53
EC2 Default User <ec2-user@ip-172-31-0-187.ec2.internal>
2021-06-03 08:48:05 +0000

    rails-4 kansei

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- get 'todolists/new'
+  get 'todolists/new'
 
  get 'top' => 'homes#top'
  post 'todolists' => 'todolists#create'
--- test/controllers/todolists_controller_test.rb
+++ test/controllers/todolists_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class TodlistsControllerTest < ActionDispatch::IntegrationTest
+class TodolistsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get todlists_new_url
+    get todolists_new_url
     assert_response :success
   end
 
